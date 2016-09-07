71517bf1852a5acb262fd8fb2b0fdc1931f5de00
ASM <cpp.create@gmail.com>
2016-09-07 16:54:11 +0300

    Added connector

---
+++ src/connector.rs
@@ -0,0 +1,13 @@
+extern crate zmq;
+
+struct Connector {
+    /*context = zmq.Context()
+    socket = self.context.socket(zmq.REQ)
+    addr = "tcp://" + ip + ":" + str(port)
+    self.socket.connect(self.addr)
+    self.t0 = time.time() - 10000
+    self.dataReady = False
+    t = str(datetime.datetime.now())
+    self.cache = None*/
+
+}
