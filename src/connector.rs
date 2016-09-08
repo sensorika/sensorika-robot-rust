e7190ab7442a113ecb4db5506deeff4aaa648d3c
ASM <cpp.create@gmail.com>
2016-09-08 09:50:00 +0300

    Connector with get, utility Trait for send/recv_json.

--- src/connector.rs
+++ src/connector.rs
@@ -1,13 +1,60 @@
-extern crate zmq;
+use std::error::Error;
+use std::result::*;
+use zmq::{Context, Socket, SocketType};
+use serde_json::{Value, Map};
+use serde_json::to_string;
+use serde_json::builder::ObjectBuilder;
+use std::fmt::format;
+use util::{now,SendRecvJson};
 
-struct Connector {
-    /*context = zmq.Context()
-    socket = self.context.socket(zmq.REQ)
-    addr = "tcp://" + ip + ":" + str(port)
-    self.socket.connect(self.addr)
-    self.t0 = time.time() - 10000
-    self.dataReady = False
-    t = str(datetime.datetime.now())
-    self.cache = None*/
+pub struct Connector {
+    context: Context,
+    socket: Socket,
+    addr: String,
+    t0: f64,
+    data_ready: bool,
+    cache: Value
+}
 
+impl Connector {
+    fn new(ip: String, port: i32) -> Result<Connector, Box<Error>> {
+        let mut context = Context::new();
+        let mut socket: Socket = try!(context.socket(SocketType::REQ));
+        let addr: String = format!("tcp://{}:{}", ip, port);
+        try!(socket.connect(&addr));
+        Ok(Connector{
+            context:context,
+            socket:socket,
+            addr: addr,
+            t0:now(), data_ready:false,
+            cache: Value::F64(0.0)})
+    }
+
+    fn from_default_port(ip: String) -> Result<Connector, Box<Error>> {
+        Connector::new(ip, 15701)
+    }
+
+    fn get_with_dt(&mut self, dt: f64) -> Result<&Value, Box<Error>> {
+        if (self.t0 + dt > now()) && self.data_ready {
+            return Ok(&self.cache)
+        }else{
+            let req = ObjectBuilder::new()
+                .insert("action", "get")
+                .insert("count", 2)
+                .build();
+            try!(self.socket.send_json(&req));
+            self.cache = try!(self.socket.recv_json());
+            self.t0 = now();
+            self.data_ready = true;
+            return Ok(&self.cache)
+        }
+    }
+
+    fn get(&mut self) -> Result<&Value, Box<Error>> {
+        self.get_with_dt(0.05)
+    }
+
+    fn set(&self, data: Value) -> Result<(), Box<Error>> {
+        Ok(())
+    }
 }
---
+++ src/util.rs
@@ -0,0 +1,33 @@
+
+
+use chrono::*;
+use zmq;
+use serde_json;
+use serde_json::Value;
+use std::str;
+use std::error::Error;
+use std::result::*;
+
+pub fn now() -> f64{
+    let local: DateTime<Local> = Local::now();
+    local.timestamp() as f64 + (local.timestamp_subsec_nanos() as f64 * 1e-9)
+}
+
+pub trait  SendRecvJson{
+    fn recv_json(&mut self) -> Result<Value, Box<Error>>;
+    fn send_json(&mut self, &Value) -> Result<(), Box<Error>>;
+}
+
+impl SendRecvJson for zmq::Socket {
+    fn recv_json(&mut self) -> Result<Value, Box<Error>> {
+        let req = try!(self.recv_bytes(0));
+        let req_slice = req.as_slice();
+        let str_req: &str =  try!(str::from_utf8(&req_slice));
+        let res: Value = try!(serde_json::from_str(&str_req));
+        Ok(res)
+    }
+
+    fn send_json(&mut self, req: &Value) -> Result<(), Box<Error>> {
+        Ok(try!(self.send(try!(serde_json::to_string(&req)).as_ref(), 0)))
+    }
+}
