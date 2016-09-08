9f691973e0ce2ae215a9d04ceb1c6f3089fb4ccf
ASM <cpp.create@gmail.com>
2016-09-08 19:43:17 +0300

    Working connector with tests

--- examples/test.rs
+++ examples/test.rs
@@ -1,13 +1,26 @@
-use std::time::SystemTime;
-use std::time::Duration;
-use std::thread;
+//FIXME: delete after prototyping
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_mut)]
+#![allow(unused_variables)]
+#![allow(unreachable_code)]
+#![feature(plugin, custom_derive)]
+#![plugin(serde_macros)]
 
+extern crate serde_json;
+extern crate zmq;
+extern crate chrono;
+extern crate sensorika;
+use sensorika::Connector;
+use sensorika::util::sendrecvjson::SendRecvJson;
+use std::thread::Builder;
+use serde_json::{Value, Map};
+use serde_json::builder::ObjectBuilder;
+use zmq::SocketType::{REQ, REP};
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main(){
-    let t = SystemTime::now();
-    loop{
-        thread::sleep_ms(500);
-        let res = t.elapsed();
-        println!("{:?}",&res);
-    }
+
+    println!("Ok");
 }
