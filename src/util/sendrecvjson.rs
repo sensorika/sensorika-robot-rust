d8a52d267ea0ed40518935a6edde1c0d4ace42fe
ASM <cpp.create@gmail.com>
2016-09-08 12:52:41 +0300

    Add test to sendrecvjson

--- src/util/sendrecvjson.rs
+++ src/util/sendrecvjson.rs
@@ -4,6 +4,7 @@ use serde_json::Value;
 use std::str;
 use std::error::Error;
 use std::result::*;
+
 pub trait  SendRecvJson{
     fn recv_json(&mut self) -> Result<Value, Box<Error>>;
     fn send_json(&mut self, &Value) -> Result<(), Box<Error>>;
@@ -20,5 +21,64 @@ impl SendRecvJson for zmq::Socket {
 
     fn send_json(&mut self, req: &Value) -> Result<(), Box<Error>> {
         Ok(try!(self.send(try!(serde_json::to_string(&req)).as_ref(), 0)))
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    use super::SendRecvJson;
+    use std;
+    use std::thread::Builder;
+    use std::sync::Arc;
+    use serde_json::{Value, Map};
+    use serde_json::builder::ObjectBuilder;
+    use zmq;
+    use zmq::SocketType::{REQ, REP};
+
+    fn handle(r: std::thread::Result<()>){
+        match r {
+            Ok(r) => println!("All is well! {:?}", r),
+            Err(e) => {
+                if let Some(e) = e.downcast_ref::<&'static str>() {
+                    panic!("Thread panicked: {}", e);
+                } else {
+                    panic!("Thread returned error: {:?}", e);
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_req_rep(){
+        let recieving = Builder::new().name("recv".into()).spawn(move||{
+            let mut ctx = zmq::Context::new();
+            let mut sock: zmq::Socket = ctx.socket(REP).unwrap();
+            sock.bind("tcp://*:5555").unwrap();
+            let req :Value = sock.recv_json().unwrap();
+            assert!(req.is_object());
+            let req : &Map<String, Value> = req.as_object().unwrap();
+            assert!(req.contains_key("action")&& req.contains_key("count"));
+            let act: &Value = req.get("action").unwrap();
+            assert!(act.is_string());
+            assert!(act.as_str().unwrap() == "set");
+            let cnt: &Value = req.get("count").unwrap();
+            assert!(cnt.as_i64().unwrap()==1);
+            let o = ObjectBuilder::new().insert("status","ok").build();
+            sock.send_json(&o);
+        }).unwrap();
+        let sending = Builder::new().name("send".into()).spawn(move||{
+            let mut ctx = zmq::Context::new();
+            let mut sock: zmq::Socket = ctx.socket(REQ).unwrap();
+            let o = ObjectBuilder::new()
+                .insert("action", "set")
+                .insert("count",1)
+                .build();
+            sock.connect("tcp://127.0.0.1:5555").unwrap();
+            sock.send_json(&o).unwrap();
+            let a:Value = sock.recv_json().unwrap();
+        }).unwrap();
+        sending.join();
+        recieving.join();
+
     }
 }
